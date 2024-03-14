1ac30199015f0cf94c927a6118374f130132e3cd
blazion09 <muhd09afiq@gmail.com>
2024-03-14 21:06:34 +0800

    feat: create card for each array

--- 08_Library/script.js
+++ 08_Library/script.js
@@ -1,5 +1,26 @@
-const myLibrary = [];
+const myLibrary = [
+  {
+    title: "book1",
+    author: "author1",
+    pages: "11",
+    status: "done",
+  },
+  {
+    title: "book2",
+    author: "author2",
+    pages: "22",
+    status: "done",
+  },
+  {
+    title: "book3",
+    author: "author3",
+    pages: "33",
+    status: "done",
+  },
+];
 // const bookDetails = ["title", "author", "pages", "status"];
+
+// const myLibrary = [];
 
 function Book(title, author, pages, status) {
   this.title = title;
@@ -36,11 +57,23 @@ document.getElementById("form").addEventListener("submit", function (event) {
 const bookDetails = ["title", "author", "pages", "status"];
 let shelve = document.querySelector(".shelve");
 
-function createDIV() {
-  bookDetails.forEach((detail) => {
-    let DOM = document.createElement("div");
-    DOM.setAttribute("id", detail);
-    DOM.classList.add("book");
-    shelve.appendChild(DOM);
+//create card and book for each array index
+function createCardDIV() {
+  myLibrary.forEach((book) => {
+    let bookNumber = "book" + myLibrary.indexOf(book);
+    let card = document.createElement("div");
+    card.setAttribute("id", bookNumber);
+    shelve.appendChild(card);
+    //add book detail to each card
+    bookDetails.forEach((detail) => {
+      let DOM = document.createElement("div");
+      DOM.setAttribute("class", detail);
+
+      //add text to each DOM
+      DOM.textContent = book[detail];
+      card.appendChild(DOM);
+    });
   });
 }
+
+function removeCardDIV() {}
