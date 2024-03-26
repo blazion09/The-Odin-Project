0e14b13220480a92e743869f20d8a3fab8846de8
blazion09 <muhd09afiq@gmail.com>
2024-03-26 15:56:58 +0800

    WIP game UI DOM

--- 09_Tic-Tac-Toe/script.js
+++ 09_Tic-Tac-Toe/script.js
@@ -31,12 +31,15 @@ function startGame() {
   const board = game.getBoard();
 
   console.table(board);
+
   console.log(`${activePlayer.name}'s turn.`);
 
   function tickTheBoard(board, row, col) {
     const mark = activePlayer.marker;
     board[row][col] = mark;
     console.table(board);
+    updateSquare(board);
+
     if (checkWin(board1)) {
       console.log(`${activePlayer.name}'s wins!`);
     } else if (checkTie(board1)) {
@@ -126,3 +129,25 @@ function checkWin(board) {
 
 const game1 = startGame();
 const board1 = game1.board;
+//gameboard DOM
+function updateSquare(board) {
+  const square1 = document.querySelector(".square-1");
+  const square2 = document.querySelector(".square-2");
+  const square3 = document.querySelector(".square-3");
+  const square4 = document.querySelector(".square-4");
+  const square5 = document.querySelector(".square-5");
+  const square6 = document.querySelector(".square-6");
+  const square7 = document.querySelector(".square-7");
+  const square8 = document.querySelector(".square-8");
+  const square9 = document.querySelector(".square-9");
+
+  square1.textContent = board[0][0];
+  square2.textContent = board[0][1];
+  square3.textContent = board[0][2];
+  square4.textContent = board[1][0];
+  square5.textContent = board[1][1];
+  square6.textContent = board[1][2];
+  square7.textContent = board[2][0];
+  square8.textContent = board[2][1];
+  square9.textContent = board[2][2];
+}
