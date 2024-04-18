b3434f6f765dbec7cf951958cfb95ed00fca9b7f
blazion09 <muhd09afiq@gmail.com>
2024-04-19 00:32:06 +0800

    chore task id logic

--- 11_To_Do/src/index.js
+++ 11_To_Do/src/index.js
@@ -1,2 +1,18 @@
 import "./css/reset.css";
 import "./css/style.css";
+
+class CreateTask {
+  constructor(title, description, priority) {
+    this.title = title;
+    this.description = description;
+    this.priority = priority;
+    this.timestamp = Date.now();
+  }
+}
+
+const task1 = new CreateTask("Task 1", "Description 1", "High");
+console.log(task1);
+
+// function generateTask( ) {
+
+// }
