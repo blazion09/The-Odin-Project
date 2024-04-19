d2fbca1f6cbde2f899fb7edfc2988ddad704fd43
blazion09 <muhd09afiq@gmail.com>
2024-04-19 11:22:11 +0800

    add task creation logic
    title, description and adding due date and priority level

--- 11_To_Do/src/index.js
+++ 11_To_Do/src/index.js
@@ -2,17 +2,23 @@ import "./css/reset.css";
 import "./css/style.css";
 
 class CreateTask {
-  constructor(title, description, priority) {
+  constructor(title, description) {
     this.title = title;
     this.description = description;
-    this.priority = priority;
     this.timestamp = Date.now();
+  }
+
+  addDueDate(DD, MM) {
+    this.dueDate = `Due on ${DD}/${MM}`;
+  }
+
+  addPriority(priorityLevel) {
+    this.priorityLevel = priorityLevel;
   }
 }
 
-const task1 = new CreateTask("Task 1", "Description 1", "High");
-console.log(task1);
+const task1 = new CreateTask("Task 1", "Description 1");
 
-// function generateTask( ) {
-
-// }
+task1.addDueDate("20", "04");
+task1.addPriority("High");
+console.dir(task1);
