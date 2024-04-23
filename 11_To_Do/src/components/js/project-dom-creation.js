003380c0c6f8d98754ae91b8b9d493eee9523be3
blazion09 <muhd09afiq@gmail.com>
2024-04-23 16:07:44 +0800

    fix duplicated DOM creation for project creation

--- 11_To_Do/src/components/js/project-dom-creation.js
+++ 11_To_Do/src/components/js/project-dom-creation.js
@@ -1,26 +1,23 @@
-import { projectDIV } from "../..";
+import { projectDIV, projectDialog, projectForm } from "../..";
 import { DOMCreation } from "./dom-creation";
 import { LocalStorage } from "./local-storage-logic";
 import { Project } from "./project-creation";
 import { taskModal } from "./task-dom-creation";
 
 export function createProject() {
-  const projectDialog = document.querySelector(".project-dialog");
   projectDialog.showModal();
+}
 
-  const projectForm = document.getElementById("project-form");
-  projectForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    //constructor(title, description)
-    const title = projectForm.elements["project-title"].value;
-    const description = projectForm.elements["project-description"].value;
+export function create() {
+  //constructor(title, description)
+  const title = projectForm.elements["project-title"].value;
+  const description = projectForm.elements["project-description"].value;
 
-    const project = new Project(title, description);
-    LocalStorage.saveProject(project);
-    projectDialog.close();
+  const project = new Project(title, description);
+  LocalStorage.saveProject(project);
+  projectDialog.close();
 
-    createDOM(project.title, description, project);
-  });
+  createDOM(project.title, description, project);
 }
 
 //create DOM for Project Title, Project Description and Create Task Button(with Data of Project name)
--- 11_To_Do/src/index.js
+++ 11_To_Do/src/index.js
@@ -4,12 +4,12 @@ import "./css/style.css";
 import taskDialogHTML from "./components/html/task-modal.html";
 import projectDialogHTML from "./components/html/project-modal.html";
 
-import { createProject } from "./components/js/project-dom-creation";
+import { create, createProject } from "./components/js/project-dom-creation";
 import { TaskDetails } from "./components/js/task-creation";
 import { Project } from "./components/js/project-creation";
 import { LocalStorage } from "./components/js/local-storage-logic";
 
-export { projectDIV };
+export { projectDIV, projectDialog, projectForm };
 
 //FOR DEVELOPMENT PROCESS
 localStorage.clear();
@@ -20,4 +20,7 @@ const projectDIV = document.querySelector(".project");
 
 //Create Project
 const createProjectBtn = document.querySelector(".create-project");
+const projectDialog = document.querySelector(".project-dialog");
+const projectForm = document.getElementById("project-form");
 createProjectBtn.addEventListener("click", createProject);
+projectForm.addEventListener("submit", create);
